576df7ceb347e7313a36351ac777bac86d2bf1cf
gabrielssmacedo <gabrielmacedo317@gmail.com>
2025-05-19 12:36:27 -0300

    build: makes Game seed

--- src/main/java/com/gabrielmacedo/dslist/entities/Game.java
+++ src/main/java/com/gabrielmacedo/dslist/entities/Game.java
@@ -20,14 +20,18 @@ public class Game implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String title;
+	private Double score;
 	
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
 	private String platforms;
-	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	//private List<GameList> gameList;
