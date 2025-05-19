f7284f5343fa8328ec8125d88405ceb4bbb49535
gabrielssmacedo <gabrielmacedo317@gmail.com>
2025-05-19 11:46:01 -0300

    Project created

---
+++ src/test/java/com/gabrielmacedo/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielmacedo.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
