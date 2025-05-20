c7edbd7987e607de4ef4a4b7e1d6ccb7d25d07cf
gabrielssmacedo <gabrielmacedo317@gmail.com>
2025-05-20 10:00:11 -0300

    feat: implements get Game by id

--- src/main/java/com/gabrielmacedo/dslist/services/GameService.java
+++ src/main/java/com/gabrielmacedo/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.gabrielmacedo.dslist.dto.GameDTO;
 import com.gabrielmacedo.dslist.dto.GameMinDTO;
 import com.gabrielmacedo.dslist.entities.Game;
 import com.gabrielmacedo.dslist.repositories.GameRepository;
@@ -15,8 +17,15 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id){
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
 	}
 }
