e151f4dd6be30790d67160e3aa4c88d85cbf6a32
gabrielssmacedo <gabrielmacedo317@gmail.com>
2025-07-08 15:56:58 -0300

    docs: add Swagger documentation

--- src/main/java/com/gabrielmacedo/dslist/controllers/GameController.java
+++ src/main/java/com/gabrielmacedo/dslist/controllers/GameController.java
@@ -2,6 +2,8 @@ package com.gabrielmacedo.dslist.controllers;
 
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,17 +16,20 @@ import com.gabrielmacedo.dslist.services.GameService;
 
 @RestController
 @RequestMapping(value = "/games")
+@Tag(name = "Games", description = "Gerenciador de games")
 public class GameController {
 	
 	@Autowired
 	private GameService gameService;
 	
 	@GetMapping
+	@Operation(summary = "Consultar games", description = "Permite a consulta de todos os games cadastrados")
 	public List<GameMinDTO> findAll() {
 		return gameService.findAll();
 	}
 	
 	@GetMapping(value = "/{id}")
+	@Operation(summary = "Consultar games por Id", description = "Permite a consulta de um game cadastrado pelo seu id")
 	public GameDTO findById(@PathVariable Long id) {
 		return gameService.findById(id);
 	}
--- src/main/java/com/gabrielmacedo/dslist/controllers/GameListController.java
+++ src/main/java/com/gabrielmacedo/dslist/controllers/GameListController.java
@@ -2,6 +2,8 @@ package com.gabrielmacedo.dslist.controllers;
 
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +20,7 @@ import com.gabrielmacedo.dslist.services.GameService;
 
 @RestController
 @RequestMapping(value = "/lists")
+@Tag(name = "Games List", description = "Gerenciador de lista de games")
 public class GameListController {
 	
 	@Autowired
@@ -27,21 +30,25 @@ public class GameListController {
 	private GameService gameService;
 	
 	@GetMapping
+	@Operation(summary = "Consultar listas", description = "Permite a consulta de todas as listas de games")
 	public List<GameListDTO> findAll() {
 		return gameListService.findAll();
 	}
 	
 	@GetMapping(value = "/{id}")
+	@Operation(summary = "Consultar lista por Id", description = "Permite a consulta de determinada lista de game pelo seu id")
 	public GameListDTO findById(@PathVariable Long id) {
 		return gameListService.findById(id);
 	}
 	
 	@GetMapping(value = "/{listId}/games")
+	@Operation(summary = "Consultar games da lista", description = "Permite a consulta de todos os games de uma determinada lista pelo seu id")
 	public List<GameMinDTO> findByList(@PathVariable Long listId) {
 		return gameService.findByList(listId);
 	}
 	
 	@PostMapping(value = "/{listId}/replacement")
+	@Operation(summary = "Trocar posição do game", description = "Permite trocar a posição de um game em sua lista")
 	public void move(@PathVariable Long listId, @RequestBody ReplacementDTO body) {
 		gameListService.move(listId, body.getSourceIndex(), body.getDestinationIndex());
 	}
